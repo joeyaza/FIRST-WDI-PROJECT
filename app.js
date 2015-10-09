bb0bdd1e09e280ec47624d58101915c966379207
joeyaza <JAzadehdel@gmail.com>
2015-10-09 19:05:01 +0100

    bub is moving

--- app.js
+++ app.js
@@ -1,18 +1,86 @@
+// $(document).ready(function(){
+
+      setInterval(function(){
+          $("#circle").fadeToggle(1000);
+      }, 200);
+
+//     setInterval(function(){
+//         $("#circle2").fadeToggle(1000);
+//     }, 600);
+
+//     setInterval(function(){
+//         $("#circle3").fadeToggle(400);
+//     }, 1000);
+// });
+
+
+$(document).ready(function(){
+    animateDiv();
+    
+});
+
+function makeNewPosition(){
+    
+    // Get viewport dimensions (remove the dimension of the div)
+    var h = $(window).height() - 50;
+    var w = $(window).width() - 50;
+    
+    var nh = Math.floor(Math.random() * h);
+    var nw = Math.floor(Math.random() * w);
+    
+    return [nh,nw];    
+    
+}
+
+function animateDiv(){
+    var newq = makeNewPosition();
+    var oldq = $('#circle').offset();
+    var speed = calcSpeed([oldq.top, oldq.left], newq);
+    
+    $('#circle').animate({ left: newq[1] }, speed, function(){
+      animateDiv();        
+    });
+    
+};
+
+function calcSpeed(prev, next) {
+    
+    var x = Math.abs(prev[1] - next[1]);
+    var y = Math.abs(prev[0] - next[0]);
+    
+    var greatest = x > y ? x : y;
+    
+    var speedModifier = 0.1;
+
+    var speed = Math.ceil(greatest/speedModifier);
+
+    return speed;
+
+}
+
+
+
+
+
+
+
+
+
 
 
 
 
 // $(document).ready(function(){
 
-// // var colour = [FFE6FF, FF99FF, FFB2FF, FFCCFF, FFB2FF, FFC2FF, FFD1FF,
-// // FFE0FF, FFF0FF, FFADFF, FFB8FF, FFC2FF, FFCCFF, FFD6FF, FFE0FF, FFEBFF, 
-// // FFF5FF, E699FF, EBADFF, F0C2FF, F5D6FF, FAEBFF, CCCCFF, D6D6FF, E0E0FF,
-// // EBEBFF, FFDBFF, FFE0FF, FFE6FF, FFEBFF, FFF0FF, FFF5FF, FFFAFF, B2B2FF,
-// // C2C2FF, D1D1FF, E0E0FF, F0F0FF, FFB8FF, FFC2FF, FFCCFF, FFD6FF,FFE0FF]
+// var colour = [FFE6FF, FF99FF, FFB2FF, FFCCFF, FFB2FF, FFC2FF, FFD1FF,
+// FFE0FF, FFF0FF, FFADFF, FFB8FF, FFC2FF, FFCCFF, FFD6FF, FFE0FF, FFEBFF, 
+// FFF5FF, E699FF, EBADFF, F0C2FF, F5D6FF, FAEBFF, CCCCFF, D6D6FF, E0E0FF,
+// EBEBFF, FFDBFF, FFE0FF, FFE6FF, FFEBFF, FFF0FF, FFF5FF, FFFAFF, B2B2FF,
+// C2C2FF, D1D1FF, E0E0FF, F0F0FF, FFB8FF, FFC2FF, FFCCFF, FFD6FF,FFE0FF]
     
-    setInterval(function(){
-        $("#1").fadeToggle(1000);
-    }, 200);
+//     setInterval(function(){
+//         $("#1").fadeToggle(1000);
+//     }, 200);
 
 // function makeNewPosition(){
     
