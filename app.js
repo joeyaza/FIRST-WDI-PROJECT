55a9646e59381c70445eb0253f4c09f6000fd05d
joeyaza <JAzadehdel@gmail.com>
2015-10-09 17:41:51 +0100

    some imp

---
+++ app.js
@@ -0,0 +1,19 @@
+var colour = [#FFE6FF, #FF99FF, #FFB2FF, #FFCCFF, #FFB2FF, #FFC2FF, #FFD1FF
+#FFE0FF,#FFF0FF, #FFADFF, #FFB8FF, #FFC2FF, #FFCCFF, #FFD6FF, #FFE0FF, #FFEBFF, 
+#FFF5FF, #E699FF, #EBADFF, #F0C2FF, #F5D6FF, #FAEBFF, #CCCCFF, #D6D6FF, #E0E0FF
+#EBEBFF, #FFDBFF, #FFE0FF, #FFE6FF, #FFEBFF, #FFF0FF, #FFF5FF, #FFFAFF, #B2B2FF
+#C2C2FF, #D1D1FF, #E0E0FF, #F0F0FF, #FFB8FF, #FFC2FF, #FFCCFF, #FFD6FF,#FFE0FF]
+
+
+
+$(document).ready(function(){
+
+
+
+    setInterval(function(){
+        $("#circle3").fadeToggle(1000);
+    }, 200);
+
+
+});
+
